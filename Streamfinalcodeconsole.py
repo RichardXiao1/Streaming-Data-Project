0be8bee7a8103c253a2d3d501093f0514617fcf1
RichardXiao1 <89166836+RichardXiao1@users.noreply.github.com>
2023-06-11 14:26:16 -0400

    Add files via upload

---
+++ Streamfinalcodeconsole.py
@@ -0,0 +1,15 @@
+
+
+#Imports pandas and reads in the power streaming data set.
+#import pandas as pd
+#power_stream = pd.read_csv("csv_streaming/power_streaming_data.csv")
+
+#imports numpy and time modules.
+#import numpy as np
+#import time
+
+#Declares for loop here to produce the csv files. Gives three observations for each csv file and the sleep method declares the pause time for 10 seconds between outputting of data sets.
+#for i in range(0,50):
+    #temp = power_stream.loc[np.random.randint(power_stream.shape[0], size = 3)]
+    #temp.to_csv("csv_streaming/power_stream" + str(i) + ".csv", index = False, header = True)
+    #time.sleep(10)
